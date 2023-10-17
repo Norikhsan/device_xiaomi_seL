7cb8dcca02aa7a2242b253e955bcd0a48f5e3f96
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 02:36:55 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,11 +21,11 @@
 FDEVICE="selene"
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-   if [ -n "$chkdev" ]; then
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -35,17 +35,22 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-        export PLATFORM_VERSION="16.1.0"
-   	export PLATFORM_SECURITY_PATCH="2099-12-31"
    	export TW_DEFAULT_LANGUAGE="en"
-   	export LC_ALL="C"
-   	export OF_AB_DEVICE=1
-   	export OF_USE_GREEN_LED=0
-   	export OF_FBE_METADATA_MOUNT_IGNORE=1
-   	export OF_USE_MAGISKBOOT=1
-   	export OF_USE_NEW_MAGISKBOOT=1
-   	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-   	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export LC_ALL="C"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+	export OF_VIRTUAL_AB_DEVICE=1
+	export OF_AB_DEVICE=1
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export OF_USE_GREEN_LED=0
+	export OF_FBE_METADATA_MOUNT_IGNORE=1
+
+	# Mediatek
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export OF_NO_MIUI_PATCH_WARNING=1
@@ -55,30 +60,20 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
+    	export OF_QUICK_BACKUP_LIST="/boot;/data;"
 	export OF_PATCH_AVB20=1
-	export FOX_DELETE_AROMAFM=1
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
-	export FOX_ENABLE_APP_MANAGER=0
-	export FOX_DISABLE_APP_MANAGER=1
-	export OF_NO_RELOAD_AFTER_DECRYPTION=1
-	export FOX_RESET_SETTINGS=disabled
+    	export FOX_DELETE_AROMAFM=1
+    	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+    	export FOX_ENABLE_APP_MANAGER=0
+    	export FOX_DISABLE_APP_MANAGER=1
 
-        # use system (ROM) fingerprint where available
-        export OF_USE_SYSTEM_FINGERPRINT=1
+    	# OTA
+    	export OF_KEEP_DM_VERITY=1
+    	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
-	# OTA for custom ROMs
-	export OF_KEEP_DM_VERITY=1
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-
-        # -- add settings for R12.1 --
-        export OF_MAINTAINER="Norikhsan"
-        export FOX_BUILD_TYPE="selene"
-        
-        
-        # screen settings
+	# screen settings
 	export OF_SCREEN_H=2340
 	export OF_STATUS_H=80
 	export OF_ALLOW_DISABLE_NAVBAR=0 # We don't have physical buttons
@@ -86,12 +81,13 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_STATUS_INDENT_RIGHT=48
 	export OF_CLOCK_POS=1
 	export OF_HIDE_NOTCH=1
-
-	# run a process after formatting data to work-around MTP issues
-	export OF_RUN_POST_FORMAT_PROCESS=1
-	export OF_SKIP_ORANGEFOX_PROCESS=1
 	
-	# senter
+	# R11.1 Settings
+	export FOX_VERSION="R12.1_0"
+	export FOX_VARIANT="A12"
+	export OF_MAINTAINER="Norikhsan"
+
+ # senter
 	export OF_FL_PATH1="/sys/class/flashlight"
 
 	# let's see what are our build VARs
@@ -102,4 +98,3 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
  	fi
 fi
-#
