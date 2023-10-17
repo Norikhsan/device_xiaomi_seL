0d78e84196ccb900ea6f2b3bc7775fcfff306138
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-17 17:05:01 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -72,3 +72,10 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
