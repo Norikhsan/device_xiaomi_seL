8e676951c4fe4bd7ab5a90a5bf9da8a276b17ff1
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-21 10:27:14 +0800

    Update and rename omni_selene.mk to twrp_selene.mk

--- twrp_selene.mk
+++ twrp_selene.mk
@@ -16,33 +16,19 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+ # Inherit from those products 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk) 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk) 
+  
+ # Inherit from hardware-specific part of the product configuration
+  $(call inherit-product, device/xiaomi/selene/device.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Inherit from selene device
-$(call inherit-product, device/xiaomi/selene/device.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
-
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
-    ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0
+ # Inherit from our custom product configuration
+  $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := omni_selene
+PRODUCT_NAME := twrp_selene
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 21061119AG
 PRODUCT_MANUFACTURER := xiaomi
