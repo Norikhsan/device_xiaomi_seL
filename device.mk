9cb3504afce01bf953a9d20b1335eb2f1bab7ee2
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-21 10:16:12 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -59,17 +59,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lptools
 
-# Additional binaries & libraries needed for recovery
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster41 \
-    libkeymaster4 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
             $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
 
