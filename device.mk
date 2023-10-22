7f1a3215436f57840f89755201880f85f90f2216
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-23 06:15:00 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -25,46 +25,64 @@
   
  # API 
  PRODUCT_SHIPPING_API_LEVEL := 30 
-  
- # V A/B 
- ENABLE_VIRTUAL_AB := true 
- $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
-  
- AB_OTA_POSTINSTALL_CONFIG += \ 
-     RUN_POSTINSTALL_system=true \ 
-     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
-     FILESYSTEM_TYPE_system=ext4 \ 
-     POSTINSTALL_OPTIONAL_system=true 
-  
- # Boot control HAL 
- PRODUCT_PACKAGES += \
-     bootctrl.selene.recovery 
-  
- # Fastbootd 
- PRODUCT_PACKAGES += \ 
-     android.hardware.fastboot@1.0-impl-mock \ 
-     android.hardware.fastboot@1.0-impl-mock.recovery \ 
-     fastbootd 
-  
- # Health Hal 
- PRODUCT_PACKAGES += \ 
-      android.hardware.health@2.1-impl \ 
-      android.hardware.health@2.1-service \ 
-      libhealthd.$(PRODUCT_PLATFORM) 
-  
- # lptool 
- PRODUCT_PACKAGES += \ 
-     lptools 
 
-PRODUCT_PACKAGES += \ 
-     otapreopt_script \ 
-     cppreopts.sh \ 
-     update_engine \ 
-     update_verifier \ 
-     update_engine_sideload 
+# V A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-PRODUCT_PACKAGES_DEBUG += \ 
-     update_engine_client 
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    vendor \
+    product \
+    system \
+    system_ext \
+    vbmeta_system \
+    vbmeta_vendor 
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-mtkimpl.recovery \
+    android.hardware.boot@1.1-mtkimpl
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+    
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Health Hal
+PRODUCT_PACKAGES += \
+     android.hardware.health@2.1-impl \
+     android.hardware.health@2.1-service \
+     libhealthd.$(PRODUCT_PLATFORM)
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
 
 # enable USB Storage
  TW_NO_USB_STORAGE := false
