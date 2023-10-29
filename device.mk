586a122571e4194757c73bf690fc388e9945042a
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-30 05:05:13 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -76,3 +76,12 @@ PRODUCT_PACKAGES += \
      android.hardware.health@2.1-impl \
      android.hardware.health@2.1-service \
      libhealthd.$(PRODUCT_PLATFORM)
+
+# Additional binaries & libraries needed for recovery 
+ TARGET_RECOVERY_DEVICE_MODULES += \ 
+     libkeymaster4 \ 
+     libpuresoftkeymasterdevice 
+  
+ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
