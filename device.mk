a69a0b101f702d8faec5fafabdffb1d1ad78514e
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-11-05 21:31:18 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
+    android.hardware.boot@1.1-mtkimpl \
     bootctrl.mt6768.recovery
 
 # Update Engine
