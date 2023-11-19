ae666829f163717a2a218436f919f4f1be4e9892
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-11-20 00:04:00 +0800

    Update twrp_selene.mk

--- twrp_selene.mk
+++ twrp_selene.mk
@@ -31,7 +31,7 @@ $(call inherit-product, device/xiaomi/selene/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := twrp_selene
+PRODUCT_NAME := omni_selene
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 21061119AG
 PRODUCT_MANUFACTURER := xiaomi
