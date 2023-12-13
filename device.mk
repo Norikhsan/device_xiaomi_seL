aca4b87295d42b639e8582541985127166275adc
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-12-13 21:50:56 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,39 +1,30 @@
- # Copyright (C) 2020 The Android Open Source Project 
- # Copyright (C) 2020 The TWRP Open Source Project 
- # Copyright (C) 2020 SebaUbuntu's TWRP device tree generator 
- # 
- # Licensed under the Apache License, Version 2.0 (the "License"); 
- # you may not use this file except in compliance with the License. 
- # You may obtain a copy of the License at 
- # 
- #     http://www.apache.org/licenses/LICENSE-2.0 
- # 
- # Unless required by applicable law or agreed to in writing, software 
- # distributed under the License is distributed on an "AS IS" BASIS, 
- # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- # See the License for the specific language governing permissions and 
- # limitations under the License. 
- # 
-  
- LOCAL_PATH := device/xiaomi/selene 
-  
-# Dynamic Partitions 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true 
-  
-# API
-PRODUCT_SHIPPING_API_LEVEL := 30
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 31
+LOCAL_PATH := device/xiaomi/selene
 
-# Virtual A/B
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual AB
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
 
-# A/B
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -41,37 +32,35 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
-    otapreopt_script
+    otapreopt_script \
+    cppreopts.sh
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
-
-# Update Engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-    
-# Fastbootd
+# fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# Health Hal
+# Health
 PRODUCT_PACKAGES += \
-     android.hardware.health@2.1-impl \
-     android.hardware.health@2.1-service \
+    android.hardware.health@1.2-impl \
+    android.hardware.health@1.2-service
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 33
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
+    libkeymaster41 \
     libpuresoftkeymasterdevice
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
