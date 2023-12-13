aff50d023bed3549ee58b086fc9fad85cb886c88
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-12-13 21:47:05 +0800

    Update twrp_selene.mk

--- twrp_selene.mk
+++ twrp_selene.mk
@@ -18,13 +18,13 @@
 
  # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from vida device
 $(call inherit-product, device/xiaomi/selene/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
