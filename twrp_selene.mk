e46de9623bb3ba1738e52683208d6c185d2f1921
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2024-02-02 17:55:05 +0800

    Update twrp_selene.mk

--- twrp_selene.mk
+++ twrp_selene.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from fleur device
+# Inherit from selene device
 $(call inherit-product, device/xiaomi/selene/device.mk)
 
 # Device identifier. This must come after all inclusions
