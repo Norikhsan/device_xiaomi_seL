8622fb2726270edefb0aec6dc5eee9f9b7842a25
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2024-02-02 17:57:12 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,79 +1,72 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-LOCAL_PATH := device/xiaomi/selene
-
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# V A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# A/B
-AB_OTA_PARTITIONS += \
-    boot \
-    vendor \
-    product \
-    system \
-    vbmeta_system \
-    vbmeta_vendor 
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-    
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
-
-# Health Hal
-PRODUCT_PACKAGES += \
-     android.hardware.health@2.1-impl \
-     android.hardware.health@2.1-service \
-     libhealthd.$(PRODUCT_PLATFORM)
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
+# 
+ # Copyright (C) 2020 The Android Open Source Project 
+ #  
+  
+ LOCAL_PATH := device/xiaomi/selene 
+  
+ # Include GSI keys 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk) 
+  
+ # Virtual AB 
+ ENABLE_VIRTUAL_AB := true 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
+  
+ # Boot control HAL 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.boot@1.2-impl \ 
+     android.hardware.boot@1.2-impl.recovery \ 
+     android.hardware.boot@1.2-service 
+  
+ PRODUCT_PACKAGES += \ 
+     update_engine \ 
+     update_engine_sideload \ 
+     update_verifier 
+  
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_system=true \ 
+     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
+     FILESYSTEM_TYPE_system=ext4 \ 
+     POSTINSTALL_OPTIONAL_system=true 
+  
+ PRODUCT_PACKAGES += \ 
+     otapreopt_script \ 
+     cppreopts.sh 
+  
+ # fastbootd 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.fastboot@1.0-impl-mock \ 
+     fastbootd 
+  
+ # Health 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.health@2.1-impl \ 
+     android.hardware.health@2.1-service 
+  
+ # Dynamic Partitions 
+ PRODUCT_USE_DYNAMIC_PARTITIONS := true 
+  
+ # VNDK 
+ PRODUCT_TARGET_VNDK_VERSION := 32 
+  
+ # API 
+ PRODUCT_SHIPPING_API_LEVEL := 31 
+  
+ # Additional Libraries 
+ TARGET_RECOVERY_DEVICE_MODULES += \ 
+     libkeymaster4 \ 
+     libkeymaster41 \ 
+     libpuresoftkeymasterdevice 
+  
+ RECOVERY_LIBRARY_SOURCE_FILES += \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
+  
+ # libion & libxml2 
+ TARGET_RECOVERY_DEVICE_MODULES += \ 
+     libion \ 
+     libxml2 
+  
+ RECOVERY_LIBRARY_SOURCE_FILES += \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
